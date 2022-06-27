b5a49f622c2f225c7bd2ea91eec7b38dda3e5795
Vini-Yking <mvdsfg@gmail.com>
2022-06-27 17:52:31 -0300

    ajustes

--- ponto1/src/Content/Conteudo.js
+++ ponto1/src/Content/Conteudo.js
@@ -1,11 +1,35 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { View, StyleSheet, Text } from "react-native";
 import { styles } from "../../styles/styles";
 
+
 export const Conteudo = () => {
+
+    useEffect(()=>{
+        criarQuadrados
+   
+   },[])
     const linhas = []
     const quadrados = []
 
+
+
+    const criarQuadrados = () => {
+        for (let i = 0; i < 3; i++) {
+            quadrados.push(
+                <View key={i} style={[styles.quadrado, { marginHorizontal: 3 }]} />
+            )
+        }
+        return quadrados
+    }
+    for (let j = 0; j < 2; j++) {
+        linhas.push(<View style={{padding:30,flexDirection: "row", justifyContent: "space-around" }}>
+            
+            {quadrados}
+        </View>
+        )
+    }
+    
     for (let i = 0; i < 3; i++) {
         quadrados.push(
             <View key={i} style={[styles.quadrado,{marginHorizontal:3}]} />
@@ -13,12 +37,7 @@ export const Conteudo = () => {
     }
     return (
         <View>
-            <View style={{ padding: 20, flexDirection: "row", justifyContent: "space-around" }}>
-                {quadrados}
-            </View>
-            <View style={{ padding: 20, flexDirection: "row", justifyContent: "space-around" }}>
-                {quadrados}
-            </View>
+           {linhas}
         </View>
 
     )
